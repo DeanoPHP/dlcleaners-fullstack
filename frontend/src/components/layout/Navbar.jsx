92e8c3208d94b8ade86f454ae35d55e2697cfe9f
DeanoLark <deanolark@gmail.com>
2024-03-13 22:22:34 +0000

    Hamburger icon

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -28,6 +28,17 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-default navbar-fixed-top navbar-dark bg-dark">
       <div id="target-nav" className="container-fluid">
         <div className="navbar-brand">Navbar</div>
+        {/* Hamburger toggle button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
@@ -63,7 +74,7 @@ function Navbar() {
                   cursor: 'pointer',
                   padding: 0,
                   marginTop: '8px',
-                  marginLeft: '10px'
+                  marginLeft: '10px',
                 }}>
                 <span
                   style={{
