8a4370b43b70de80db49771780829091018f9806
DeanoLark <deanolark@gmail.com>
2024-03-13 21:27:53 +0000

    settings for push to heruku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv').config();
 const connect = require('./config/db');
@@ -7,17 +8,23 @@ connect()
 
 const app = express()
 
-app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: 'Welcome to Dee Massages'
-    })
-})  
+app.use(express.json()) 
 
 app.use('/api/v1/users', require('./routes/userRoutes'))
 app.use('/api/v1/bookings', require('./routes/bookingRoutes'))
 app.use('/api/v1/timeslot', require('./routes/dateTimeslots'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({
+            success: true,
+            msg: 'Welcome to Dee Massages'
+        })
+    }) 
+}
     
 app.listen(PORT, () => console.log(`You are listening on port ${PORT}`))
